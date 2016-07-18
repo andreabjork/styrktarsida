1c3937df14d4c6e056304e202a0a8a4ba791411d
andreabjork <andreabjork43@gmail.com>
2016-07-18 17:10:40 +0000

    Email test, needs to be put up on godaddy

--- DalPayipn.php
+++ DalPayipn.php
@@ -1,2 +1,13 @@
 <!-- success -->
 <button onclick="window.location.href='http://educationinasuitcase.com/donation'">Til baka á <italic>Education in a Suitcase</italic></button>
+
+<?php
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("andreabjork43@gmail.com","EIAS Donation",$msg);
+?> 
