bfa63e35045e559e69f9584842276a929ae134b7
Andrea Björk <andreabjork43@gmail.com>
2016-07-21 11:22:21 +0000

    Stylesheeting

--- sendEmail.php
+++ sendEmail.php
@@ -1,14 +1,29 @@
 <?php
 $msg = "";
-if(isset($_POST['smly_addr']) && isset($_POST['remote_addr']) && isset($_POST['order_datetime']) && isset($_POST['total_amount'])) {
-	$msg = $msg . "wallet:" . $_POST['smly_addr'] . "\n"
-				. "ip:" . $_POST['remote_addr'] . "\n" 
-				. "date:" . $_POST['order_datetime'] . "\n" 
-				. "amount:" . $_POST['total_amount'] . "\n"
-				. "email:" . $_POST['email_addr'];
+// Handle smly_addr and email_addr separately because
+// user might in some out-dated browsers get past the required fields.
+if(isset($_POST['smly_addr'])) {
+	$msg = $msg . "wallet:" . $_POST['smly_addr'] . "\n";
 } else {
-	$msg = $msg . "Something went wrong when getting one or more of the parameters: smly_addr, remote_Addr, order_datetime, total_amount";
+	$msg = $msg . "wallet:none" . "\n";
 }
+
+if(isset($_POST['smly_addr']) && isset($_POST['remote_addr']) && isset($_POST['order_datetime']) && isset($_POST['total_amount'])) {
+	$msg = $msg . "ip:" . $_POST['remote_addr'] . "\n" 
+				. "date:" . $_POST['order_datetime'] . "\n" 
+				. "amount:" . $_POST['total_amount'] . "\n";
+
+	// Nest this so we can completely override the mail if we don't get the necessary params:
+	if(isset($_POST['email_addr'])) {
+		$msg = $msg . "email:" . $_POST['email_addr'];
+	} else {
+		$msg = $msg . "email:none@none.com";
+	}
+} else {
+	$msg = "ERROR: Something went wrong when getting one or more of the parameters: smly_addr, remote_Addr, order_datetime, total_amount";
+}
+
+
 
 $msg = wordwrap($msg,70);
 
